65ddc6c7ad771adf07057c38a964bdd11f7d4de5
Songqing Sun <sunsq1991@gmail.com>
2025-02-27 15:10:59 +0800

    update

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,8 +3,10 @@ export default ({ env }) => ({
       config: {
         provider: 'aws-s3', // For community providers pass the full package name (e.g. provider: 'strapi-provider-upload-google-cloud-storage')
         providerOptions: {
-          accessKeyId: env('AWS_ACCESS_KEY_ID'),
-          secretAccessKey: env('AWS_ACCESS_SECRET'),
+          credentials: {
+            accessKeyId: env('AWS_ACCESS_KEY_ID'),
+            secretAccessKey: env('AWS_ACCESS_SECRET'),
+          },
           region: 'ca-central-1',
           params: {
             ACL: env('AWS_ACL', 'public-read'), // 'private' if you want to make the uploaded files private
